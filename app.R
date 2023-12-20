22a21e09d73ff2fedbba360f760cb789cd5c39c6
RaulMoldes <raul.moldes.work@gmail.com>
2023-12-20 18:11:33 +0100

    IDIOM 4

--- app.R
+++ app.R
@@ -2,10 +2,8 @@ library(shiny)
 library(plotly)
 
 # Assuming your data is in a CSV file named 'PlayerAttributeData.csv'
-data <- read.csv("data/PlayerAttributeData.csv")
-data2 <- read.csv("data/PlayerPersonalData.csv")
-data <- as.data.frame(lapply(data,as.numeric))
-data <- na.omit(data)
+data <-read.csv('data/CompleteDatasetFilteredFinal.csv')
+data <- as.data.frame(data)
 
 
 ui <- fluidPage(
@@ -13,38 +11,45 @@ ui <- fluidPage(
   titlePanel("Football Statistics Parallel Chart"),
   sidebarLayout(
     sidebarPanel(
-      selectInput("player", "Choose a player:", choices = unique(data2$Club))
+      selectInput("position", "Choose a position:", choices = unique(data$PreferredPositions))
     ),
-  mainPanel(
-    plotlyOutput("ParallelChart")
+    mainPanel(
+      plotlyOutput("StatsPlot") 
+    )
   )
-)
 )
 
 server <- function(input, output) {
-  # Filter based on the selected club
-  data_filtered <- reactive({data[data$ID == data2$ID[data2$Club == input$player],]})
-  df <- reactive({data_filtered()})
-  output$ParallelChart <- renderPlotly({
+  data_filtered <- reactive({
+    df <- data[data$PreferredPositions == input$position,]
+    print(df$Name)
+    df
+  })
+  
+  output$StatsPlot <- renderPlotly({
     df <- data_filtered()
-    parallel_chart <- plot_ly(
+    stats_plot <- plot_ly(
       type = 'parcoords',
-      line = list(color = 'blue'),
+      line = list(color = 'purple'),
       dimensions = list(
-        list(range= c(0,100),label = 'Acceleration', values = df$Acceleration),
-        list(range= c(0,100),label = 'Aggression', values = df$Aggression),
-        list(range = c(0,100),label = 'Agility', values = df$Agility),
-        list(range = c(0,100),label = 'Balance', values = df$Balance),
-        list(range = c(0,100),label = 'Ball control', values = df$Ball.control),
-        list(range = c(0,100),label = 'Composure', values = df$Composure),
-        list(range = c(0,100),label = 'Crossing', values = df$Crossing),
-        list(range = c(0,100),label = 'Dribbling', values = df$Dribbling),
-        list(range = c(0,100),label = 'Curve', values = df$Curve),
-        list(range = c(0,100),label = 'Finishing', values = df$Finishing)
+        list(range = c(0,100), label = 'Acceleration', values = df$Acceleration),
+        list(range = c(0,100), label = 'Aggression', values = df$Aggression),
+        list(range = c(0,100), label = 'GK Reflexes',values = df$GK.reflexes),
+        list(range = c(0,100), label = 'Agility', values = df$Agility),
+        list(range = c(0,100), label = 'Crossing', values = df$Crossing),
+        list(range = c(0,100), label = 'GK Positioning', values = df$GK.positioning),
+        list(range = c(0,100), label = 'Ball control', values = df$Ball.control),
+        list(range = c(0,100), label = 'Acceleration', values = df$Acceleration),
+        list(range = c(0,100), label = 'Speed', values = df$Sprint.speed),
+        list(range = c(0,100), label = 'Heading', values = df$Heading.accuracy),
+        list(range = c(0,100), label = 'Finishing', values = df$Finishing),
+        list(range = c(0,100), label = 'Interceptions', values = df$Interceptions)
+        
       )
     )
-    parallel_chart
+    stats_plot
   })
+
 }
 
 shinyApp(ui, server)
