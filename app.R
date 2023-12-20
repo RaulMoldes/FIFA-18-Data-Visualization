2aef4e69e82262bf25bee4b2b7e774e1902d7c67
RaulMoldes <raul.moldes.work@gmail.com>
2023-12-20 19:30:08 +0100

    IDIOM 3

--- app.R
+++ app.R
@@ -5,6 +5,9 @@ library(plotly)
 data <-read.csv('data/CompleteDatasetFilteredFinal.csv')
 data <- as.data.frame(data)
 
+stats <- c('Acceleration','Aggression','Agility','Balance','Ball control','Composure','Crossing','Curve','Dribbling','Finishing','Free kick accuracy','GK diving','GK handling','GK kicking','GK positioning','GK reflexes','Heading accuracy','Interceptions','Jumping','Long passing','Long shots','Marking','Penalties','Positioning','Reactions','Short passing','Shot power','Sliding tackle','Sprint speed','Stamina','Standing tackle','Strength','Vision','Volleys')
+
+
 
 ui <- fluidPage(
   
@@ -16,7 +19,17 @@ ui <- fluidPage(
     mainPanel(
       plotlyOutput("StatsPlot") 
     )
-  )
+  ), titlePanel("Cluster Analysis Wage - Stats"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("x_stat", "Choose a stat for X axis:", choices = stats),
+      selectInput("y_stat", "Choose a stat for Y axis:", choices = stats),
+      selectInput("z_stat", "Choose a stat for Z axis:", choices = stats)
+    ),
+    mainPanel(
+      plotlyOutput("ClustersPlot") 
+    )
+)
 )
 
 server <- function(input, output) {
@@ -49,8 +62,26 @@ server <- function(input, output) {
     )
     stats_plot
   })
-
+  
+  output$ClustersPlot <- renderPlotly({
+    df <- data
+  
+    
+    # Crear rangos para la variable wage
+    df$wage_range <- df$Wage
+    
+    
+    clusters_plot <- plot_ly(df, x = ~df[[input$x_stat]], y = ~df[[input$y_stat]], z = ~df[[input$z_stat]], color = ~wage_range, colors = 'Blues') %>%
+      add_markers() %>%
+      layout(scene = list(xaxis = list(title = input$x_stat),
+                          yaxis = list(title = input$y_stat),
+                          zaxis = list(title = input$z_stat)))
+    clusters_plot
+  })
 }
+
+
+
 
 shinyApp(ui, server)
 
